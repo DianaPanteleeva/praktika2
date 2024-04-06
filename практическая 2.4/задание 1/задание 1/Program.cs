27abe6cc88ddef7773344ea06f9060e2fb570bbe
DianaPanteleeva <153964992+DianaPanteleeva@users.noreply.github.com>
2024-04-06 10:40:55 +0700

    Add files via upload

---
+++ практическая 2.4/задание 1/задание 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace задание_1
+{
+    class RomanNumerals
+    {
+        public int RomanToInt(string n)
+        {
+            Dictionary<char, int> RomanNum = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+
+            };
+            int result = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (i < n.Length - 1 && RomanNum[n[i]] < RomanNum[n[i + 1]])
+                {
+                    result -= RomanNum[n[i]];
+                }
+                else
+                {
+                    result += RomanNum[n[i]];
+                }
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            RomanNumerals romanNumerals = new RomanNumerals();
+            Console.WriteLine($"III = {romanNumerals.RomanToInt("IIVVVI")}" );
+        }
+    }
+}
